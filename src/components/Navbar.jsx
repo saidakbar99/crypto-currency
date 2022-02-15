0fd6aa96185334849397f72507e572590fccb3c6
zafarov_saidakbar <s.zafarov@student.inha.uz>
2022-02-15 14:11:20 +0500

    final eskizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ export default function Navbar(){
             <div className='logo-container'>
                 <Avatar src={icon} size='large' />
                 <Typography.Title level={2} className='logo'>
-                    <Link to='/'>Cryptoverse</Link>
+                    <Link to='/'>Saidakbar's <br /> Crypto Market</Link>
                 </Typography.Title>
                 <Button className='menu-control-container' onClick={() => setActiveMenu(!activeMenu)}>
                     <MenuOutlined />
